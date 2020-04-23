49cb140317a095a03274131db9d5642472b13723
Ashley Marques <marques.ashley@gmail.com>
2020-04-23 18:24:36 -0300

    Updates

---
+++ precache-manifest.fc06a98e06e772784e20f5cc5ad02287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962971b9528dc2acd4d7c22504ebe4be",
+    "url": "/leonibr/react-calculator/index.html"
+  },
+  {
+    "revision": "8d9c73c05c8bfc1f548a",
+    "url": "/leonibr/react-calculator/static/css/main.f7ad6c31.chunk.css"
+  },
+  {
+    "revision": "017ae1379304ff85e824",
+    "url": "/leonibr/react-calculator/static/js/2.f6a01af3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/leonibr/react-calculator/static/js/2.f6a01af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d9c73c05c8bfc1f548a",
+    "url": "/leonibr/react-calculator/static/js/main.06356e6f.chunk.js"
+  },
+  {
+    "revision": "38239bd0c3eaa96abcd4",
+    "url": "/leonibr/react-calculator/static/js/runtime-main.04851f4b.js"
+  }
+]);
